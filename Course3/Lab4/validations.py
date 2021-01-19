7fe2057d83a536095f5bf8002345a422f2060b5e
Homan_F8 <homanfarahmand@yahoo.com>
2021-01-19 16:43:38 +0000

    Modified a line and adjusted the regex to the required requirement in two conditions

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,12 +13,13 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if re.match('^[a-z1-9][a-z1-9_.]*[a-z1-9]$', username):
+        return True
+    if not re.match('^[a-z1-9][a-z1-9_.]*[a-z1-9]$', username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    return True
 
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
